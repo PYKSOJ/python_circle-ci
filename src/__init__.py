0aa9d4900deff0014011e8558d2fa880eb4e131e
PYKSOJ <6210110216@psu.ac.th>
2022-09-18 22:47:20 +0700

    init src

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+from src.number_utils import is_even_number
+
+__all__ = [is_even_number]
---
+++ src/number_utils.py
@@ -0,0 +1,6 @@
+def is_even_number(number):
+    if number % 2 == 0:
+        return 'even number'
+    else:
+        return 'odd number'
+    
